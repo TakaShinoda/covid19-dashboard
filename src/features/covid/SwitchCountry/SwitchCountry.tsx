3db0b36a3aa94e689c8a8ff55a94b1978b43ed31
Takahiro <tkhr_621@docomo.ne.jp>
2020-09-12 20:27:58 +0900

    国別選択

---
+++ src/features/covid/SwitchCountry/SwitchCountry.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { NativeSelect, FormControl } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { fetchAsyncGetCountry } from '../covidSlice'
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    marginBottom: theme.spacing(3),
+    minWidth: 320,
+  },
+}))
+
+export const SwitchCountry: FC = () => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+
+  const countries = [
+    'japan',
+    'china',
+    'us',
+    'france',
+    'italy',
+    'spain',
+    'united kingdom',
+    'germany',
+    'russia',
+    'brazil',
+    'taiwan',
+    'thailand',
+    'new zealand',
+    'sweden',
+    'india',
+  ]
+  return (
+    <FormControl className={classes.formControl}>
+      <NativeSelect
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          dispatch(fetchAsyncGetCountry(e.target.value))
+        }
+      >
+        <option value="">Worldwide</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  )
+}
